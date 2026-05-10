2cb1736326ad9cf42007afa8b1f91b9b57bd6140
cq <2068309821@qq.com>
2026-05-10 16:30:33 +0800

    文件传输任务代码

--- my-panel-ui/jest.config.js
+++ my-panel-ui/jest.config.js
@@ -1,14 +1,27 @@
 export default {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.jsx?$': ['babel-jest', { presets: [['@babel/preset-env', { targets: { node: 'current' } }]] }]
+    '^.+\\.jsx?$': ['babel-jest', { 
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-react'
+      ]
+    }]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   moduleFileExtensions: ['js', 'jsx'],
   testMatch: ['**/__tests__/**/*.(test|spec).(js|jsx)'],
   collectCoverageFrom: [
-    'src/pages/batch/**/*.js',
-    '!src/pages/batch/**/__tests__/**'
+    'src/pages/batch/**/*.{js,jsx}',
+    'src/api/batch/**/*.js',
+    '!src/pages/batch/**/__tests__/**',
+    '!src/api/batch/**/__tests__/**'
   ],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov']
+  coverageReporters: ['text', 'lcov'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios)/)'
+  ]
 };

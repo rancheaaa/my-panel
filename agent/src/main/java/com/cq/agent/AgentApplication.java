2091cbfbeb1dacf1a2e1972210a137a0a6340c2d
cq <2068309821@qq.com>
2026-06-01 17:09:05 +0800

    agent打包成可执行程序

--- agent/src/main/java/com/cq/agent/AgentApplication.java
+++ agent/src/main/java/com/cq/agent/AgentApplication.java
@@ -1,24 +1,42 @@
 package com.cq.agent;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.util.StatusPrinter2;
 import com.cq.agent.di.AgentBootstrap;
 import com.cq.agent.di.AgentModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Objects;
 
 public class AgentApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(AgentApplication.class);
-
     public static void main(String[] args) {
         try {
+            initLogback();
+            LoggerFactory.getLogger(AgentApplication.class);
             Injector injector = Guice.createInjector(new AgentModule());
             AgentBootstrap bootstrap = injector.getInstance(AgentBootstrap.class);
             bootstrap.start();
         } catch (Exception e) {
-            logger.error("Failed to start agent", e);
+            System.err.println("Failed to start agent: " + e.getMessage());
             System.exit(1);
         }
+    }
+
+    private static void initLogback() {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.reset();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(loggerContext);
+        String configPath = System.getProperty("logback.configurationFile", "logback.xml");
+        try {
+            configurator.doConfigure(Objects.requireNonNull(AgentApplication.class.getClassLoader().getResource(configPath)));
+        } catch (Exception e) {
+            System.err.println("Failed to load logback config: " + configPath + ", using default");
+        }
+        final StatusPrinter2 statusPrinter2 = new StatusPrinter2();
+        statusPrinter2.printInCaseOfErrorsOrWarnings(loggerContext);
     }
 }
